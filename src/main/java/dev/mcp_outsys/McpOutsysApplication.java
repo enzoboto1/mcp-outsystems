7a7787e1d306fb1adf3e6c4c03fad0411e2d3632
enzoboto1 <enzoboto1.0@gmail.com>
2025-04-16 19:04:54 -0300

    Disponibilizando @Bean outsysTools na classe McpOutsysApplication para retornar tools disponiveis

--- src/main/java/dev/mcp_outsys/McpOutsysApplication.java
+++ src/main/java/dev/mcp_outsys/McpOutsysApplication.java
@@ -1,13 +1,23 @@
 package dev.mcp_outsys;
 
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class McpOutsysApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(McpOutsysApplication.class, args);
+    }
+
+    @Bean
+    public List<ToolCallback> outsysTools(UsuarioService usuarioService) {
+        return List.of(ToolCallbacks.from(usuarioService));
     }
 
 }
