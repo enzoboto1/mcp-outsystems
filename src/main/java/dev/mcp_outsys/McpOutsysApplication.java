62ad5fceed05f2e615d44a87c8b4fe0235107d70
enzoboto1 <enzoboto1.0@gmail.com>
2025-04-16 18:57:57 -0300

    Initial commit

---
+++ src/main/java/dev/mcp_outsys/McpOutsysApplication.java
@@ -0,0 +1,13 @@
+package dev.mcp_outsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpOutsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpOutsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mcp_outsys/McpOutsysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mcp_outsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpOutsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
